bb0a5e06e07665900ee12b82c4c1826f4f78ac32
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2024-05-07 03:28:14 +0000

    Added Recipe detail

--- src/app/recipes/[id]/page.tsx
+++ src/app/recipes/[id]/page.tsx
@@ -9,7 +9,9 @@ import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 
 import { ref } from 'firebase/storage';
-
+import { PiBowlFood, PiHeart, PiShare, PiStar, PiTimer, PiTrash } from 'react-icons/pi';
+import { FaRegEdit } from "react-icons/fa";
+import Link from 'next/link';
 
 
 
@@ -62,81 +64,94 @@ const RecipeDetail = ({params}: {params: {id: string}}) => {
 
 
 
-    const handleSubmit = async (e:any) => {
-        e.preventDefault();
-        const recipe = doc(db, "recipes", params.id);
-        await updateDoc(recipe, {
-            title: title,
-            ingredients: ingredients,
-            instructions: instructions,
-            cookTime: cookTime
-        })
-        router.push("/recipes");
-    };
 
    
 
   return (
-    <div className='flex items-center w-full justify-around h-full box-border' >
-        <div className='w-2/5 p-0 box-border h-5/6 rounded-2xl border-8' >
-            <Image src={file} width={500} height={500} className='rounded-2xl' alt='' style={{"width": "100%", "height": "100%", "objectFit": "cover"}}/>
-        </div>
-        <form onSubmit={handleSubmit} className='p-2 flex gap-1  w-2/5 box-border flex-wrap' style={{"height": "fit-content"}}>
-        <h1 className="mb-3 text-center w-full box-border text-2xl">View/Update Recipe</h1>
-         
-            <div className='mb-1 w-full box-border border-red'>
-            <span className="text-orange-600">Name of Recipe: </span>
-                <input
-                    className='w-full p-2 rounded'
-                    type="text" value={title} 
-                    onChange={e => setTitle(e.target.value)}
-                    placeholder='title' 
-                    required
-                    disabled={!session.data?.user}
-                    style={{"border": "1px solid grey"}}
-                />
-            </div>
-            <div className='mb-1 w-full box-border'>
-            <span className="text-orange-600">Ingredients: </span>
-                <textarea
-                    className='w-full p-2 rounded'
-                    value={ingredients} 
-                    onChange={e => setIngredients(e.target.value)}
-                    placeholder='ingredients'
-                    rows={3}
-                    required 
-                    disabled={!session.data?.user}
-                    style={{"border": "1px solid grey"}}
-                ></textarea>
-            </div>
-            <SimpleMDE value={instructions} 
-                    className='w-8/12'
-                    onChange={(newValue) => setInstructions(newValue)}
-                    placeholder='Instructions'
-                
-                    aria-disabled={!session.data?.user}
-                    />
-            
+   
 
-            <div className='mb-2 ml-2 w-3/12 box-border'>
-                <span className="text-orange-600">Cook Time: </span>
-                <input
-                    className='w-full p-2 rounded'
-                    type="number" value={cookTime} 
-                    onChange={e => setcookTime(e.target.value)}
-                    placeholder='Cook Time (minutes)' 
-                    required
-                    disabled={!session.data?.user}
-                    style={{"border": "1px solid grey"}}
-                />
-            </div>
-            
-            <button className='flex h-fit bg-blue-600 p-2 px-4 rounded text-white'>Update</button>
-            {session.data?.user ? <button onClick={handleDelete} className="flex h-fit items-center ml-6 bg-red-500 p-2 px-4 rounded text-white">
-                Delete Recipe
-            </button> : null}
-        </form> 
+<div className="recipe-detail-container">
+        
+<div className="recipe-first-sec">
+    <div className="image-box">
+    <Image src={file} width={500} height={500} alt='' />
+
+        <span className="fav-btn">
+            <PiHeart className='card-icons'/>
+        </span>
     </div>
+    <div className="card-content">
+       
+        <h3 className="recipe-title">{title}</h3>
+        {session.data?.user ? <div className="action-section">
+            <a href="#"><PiStar className='action-icon'/>Rate</a>
+            <a href="#"><PiShare className='action-icon'/>Share</a>
+            <Link href={"/recipes/" + (params.id) + "/update"}><FaRegEdit className='action-icon'/>Edit</Link>
+            <button onClick={handleDelete}><PiTrash className='action-icon'/>Delete</button>
+        </div> : null}
+       
+        <div className="more-details">
+            <p className="serves">
+                <PiBowlFood className='card-icons'/>
+                <span>1 Bowl</span>
+            </p>
+            <p className="cook-time">
+                <PiTimer className='card-icons'/>
+                <span>Cook: {cookTime} mins</span>
+            </p>
+            <p className="ratings">
+                <PiStar className='card-icons'/>
+                <PiStar className='card-icons'/>
+                <PiStar className='card-icons'/>
+                <PiStar className='card-icons'/>
+                <PiStar className='card-icons'/>
+                
+            </p>
+        </div>
+        <div className="meal-type-tags">
+            <span>Appetizers</span>
+            <span>Dinner</span>
+            <span>Lunch</span>
+            <span>Soup</span>
+            <span>Vegetarian</span>
+
+        </div>
+
+        <div className="recipe-description">
+            <h4>Description</h4>
+            <hr/>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed quos ratione ipsa harum architecto amet, consequuntur libero officiis ea eligendi? Illum perspiciatis ipsa fuga, explicabo, at asperiores ratione aliquid mollitia molestias a, architecto ipsam aperiam.</p>
+        </div>
+    </div>
+</div>
+<div className="recipe-second-sec">
+    <div className="recipe-ingredients">
+        <h4>Ingredients</h4>
+        <hr/>
+        <div className="ingred-container">
+            <span>{ingredients}</span>
+            {/* <span><b className="quant">2</b> tsp acai powder</span>
+            <span><b className="quant">1</b> handful blueberries</span>
+            <span><b className="quant">1</b> medium banana</span>
+            <span><b className="quant">3/4</b> cup almond, or milk of your choice</span> */}
+        </div>
+    </div>
+    <div className="recipe-instructions">
+        <h4>Instructions</h4>
+        <hr/>
+        <div className="instruct-container">
+            {/* <span><b className="quant">1</b> Put all the smoothie ingredients into a blender.</span>
+            <span><b className="quant">2</b> If the mixture is having trouble blending, add more Almond Milk or water</span>
+            <span><b className="quant">3</b> Blend for 2-3 minutes or until the smoothie has no lumps. Pour the smoothie mixture into a bowl.</span>
+            <span><b className="quant">4</b> Top with desired ingredients</span>
+            <span><b className="quant">5</b> Serve and enjoy!</span> */}
+
+            <span>{instructions}</span> 
+                   
+        </div>
+    </div>
+</div>
+</div>
   )
 }
 
---
+++ src/app/recipes/[id]/update/page.tsx
@@ -0,0 +1,143 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import { auth, db, storage } from '@/app/firebase';
+import { useSession } from 'next-auth/react';
+import { deleteDoc, doc, getDoc, updateDoc } from 'firebase/firestore';
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+
+import { ref } from 'firebase/storage';
+
+
+
+
+const RecipeUpdate = ({params}: {params: {id: string}}) => {
+  
+    const router = useRouter()
+    const [title, setTitle] = useState("");
+    const [ingredients, setIngredients] = useState("");
+    const [instructions, setInstructions] = useState("");
+    const [cookTime, setcookTime] = useState("");
+    const [file, setFile] = useState("")
+    const [userId, setuserId] = useState("")
+    const session  = useSession();
+    
+    useEffect(() => {
+        
+        const getRecipeDetails = async () => {
+            try {
+                const recipeDoc = doc(db, 'recipes', params.id);
+                const recipeSnapshot = await getDoc(recipeDoc);
+                const data = recipeSnapshot.data();
+                if (data) {
+                    setTitle(data.title ?? "");
+                    setIngredients(data.ingredients ?? "");
+                    setInstructions(data.instructions ?? "");
+                    setcookTime(data.cookTime ?? 0);
+                    setFile(data.imageUrl ?? null);
+                    setuserId(data.userId);
+                }
+                
+            } catch (error) {
+                console.error('Error fetching recipe details:', error);
+            }
+        };
+        getRecipeDetails();
+    }, [params.id])
+
+    //Handle Delete
+    const handleDelete = async () => {
+        try {
+          const recipeDoc = doc(db, 'recipes', params.id);
+          await deleteDoc(recipeDoc);
+          router.push('/');
+        } catch (error) {
+          console.error('Error deleting recipe:', error);
+        }
+      };
+    
+    
+
+
+
+    const handleSubmit = async (e:any) => {
+        e.preventDefault();
+        const recipe = doc(db, "recipes", params.id);
+        await updateDoc(recipe, {
+            title: title,
+            ingredients: ingredients,
+            instructions: instructions,
+            cookTime: cookTime
+        })
+        router.push("/recipes");
+    };
+
+   
+
+  return (
+    <div className='flex items-center w-full justify-around h-full box-border' >
+        <div className='w-2/5 p-0 box-border h-5/6 rounded-2xl border-8' >
+            <Image src={file} width={500} height={500} className='rounded-2xl' alt='' style={{"width": "100%", "height": "100%", "objectFit": "cover"}}/>
+        </div>
+        <form onSubmit={handleSubmit} className='p-2 flex gap-1  w-2/5 box-border flex-wrap' style={{"height": "fit-content"}}>
+        <h1 className="mb-3 text-center w-full box-border text-2xl">View/Update Recipe</h1>
+         
+            <div className='mb-1 w-full box-border border-red'>
+            <span className="text-orange-600">Name of Recipe: </span>
+                <input
+                    className='w-full p-2 rounded'
+                    type="text" value={title} 
+                    onChange={e => setTitle(e.target.value)}
+                    placeholder='title' 
+                    required
+                    disabled={!session.data?.user}
+                    style={{"border": "1px solid grey"}}
+                />
+            </div>
+            <div className='mb-1 w-full box-border'>
+            <span className="text-orange-600">Ingredients: </span>
+                <textarea
+                    className='w-full p-2 rounded'
+                    value={ingredients} 
+                    onChange={e => setIngredients(e.target.value)}
+                    placeholder='ingredients'
+                    rows={3}
+                    required 
+                    disabled={!session.data?.user}
+                    style={{"border": "1px solid grey"}}
+                ></textarea>
+            </div>
+            <SimpleMDE value={instructions} 
+                    className='w-8/12'
+                    onChange={(newValue) => setInstructions(newValue)}
+                    placeholder='Instructions'
+                
+                    aria-disabled={!session.data?.user}
+                    />
+            
+
+            <div className='mb-2 ml-2 w-3/12 box-border'>
+                <span className="text-orange-600">Cook Time: </span>
+                <input
+                    className='w-full p-2 rounded'
+                    type="number" value={cookTime} 
+                    onChange={e => setcookTime(e.target.value)}
+                    placeholder='Cook Time (minutes)' 
+                    required
+                    disabled={!session.data?.user}
+                    style={{"border": "1px solid grey"}}
+                />
+            </div>
+            
+            <button className='flex h-fit bg-blue-600 p-2 px-4 rounded text-white'>Update</button>
+            {session.data?.user ? <button onClick={handleDelete} className="flex h-fit items-center ml-6 bg-red-500 p-2 px-4 rounded text-white">
+                Delete Recipe
+            </button> : null}
+        </form> 
+    </div>
+  )
+}
+
+export default RecipeUpdate
